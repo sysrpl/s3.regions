0a304616d44f64ab6b39ea1a5f3f491eefef335c
Anthony Walter <sysrpl@gmail.com>
2023-10-06 09:48:38 -0400

    Added amazon s3 regions

---
+++ services.js
@@ -0,0 +1,127 @@
+{
+    "amazon": {
+        "host": "amazonaws.com",
+        "service": "s3",
+        "accessId": "AWS_ACCESS_KEY_ID",
+        "secretKey": "AWS_SECRET_ACCESS_KEY",
+        "defaultRegion": "us-east-1",
+        "regions": [
+            {
+                "name": "US East (N. Virginia)",
+                "value": "us-east-1"
+            },
+            {
+                "name": "US East (Ohio)",
+                "value": "us-east-2"
+            },
+            {
+                "name": "US West (N. California)",
+                "value": "us-west-1"
+            },
+            {
+                "name": "US West (Oregon)",
+                "value": "us-west-2"
+            },
+            {
+                "name": "Africa (Cape Town)",
+                "value": "af-south-1"
+            },
+            {
+                "name": "Asia Pacific (Mumbai)",
+                "value": "ap-south-1"
+            },
+            {
+                "name": "Asia Pacific (Hyderabad)",
+                "value": "ap-south-2"
+            },
+            {
+                "name": "Asia Pacific (Singapore)",
+                "value": "ap-southeast-1"
+            },
+            {
+                "name": "Asia Pacific (Jakarta)",
+                "value": "ap-southeast-3"
+            },
+            {
+                "name": "Asia Pacific (Melbourne)",
+                "value": "ap-southeast-4"
+            },
+            {
+                "name": "Asia Pacific (Tokyo)",
+                "value": "ap-northeast-1"
+            },
+            {
+                "name": "Asia Pacific (Sydney)",
+                "value": "ap-southeast-2"
+            },
+            {
+                "name": "Asia Pacific (Seoul)",
+                "value": "ap-northeast-2"
+            },
+            {
+                "name": "Asia Pacific (Osaka)",
+                "value": "ap-northeast-3"
+            },
+            {
+                "name": "Canada (Central)",
+                "value": "ca-central-1"
+            },
+            {
+                "name": "China (Beijing)",
+                "value": "cn-north-1"
+            },
+            {
+                "name": "China (Ningxia)",
+                "value": "cn-northwest-1"
+            },
+            {
+                "name": "Europe (Frankfurt)",
+                "value": "eu-central-1"
+            },
+            {
+                "name": "Europe (Ireland)",
+                "value": "eu-west-1"
+            },
+            {
+                "name": "Europe (London)",
+                "value": "eu-west-2"
+            },
+            {
+                "name": "Europe (Paris)",
+                "value": "eu-west-3"
+            },
+            {
+                "name": "Europe (Milan)",
+                "value": "eu-south-1"
+            },
+            {
+                "name": "Europe (Spain)",
+                "value": "eu-south-2"
+            },
+            {
+                "name": "Europe (Zurich)",
+                "value": "eu-central-2"
+            },
+            {
+                "name": "Europe (Stockholm)",
+                "value": "eu-north-1"
+            },
+            {
+                "name": "South America (São Paulo)",
+                "value": "sa-east-1"
+            },
+            {
+                "name": "Middle East (Bahrain)",
+                "value": "me-south-1"
+            },
+            {
+                "name": "Middle East (UAE)",
+                "value": "me-central-1"
+            },
+            {
+                "name": "Israel (Tel Aviv)",
+                "value": "il-central-1"
+            }
+        ]
+    }
+}
